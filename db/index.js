a8d13865ce2ba24d2fd966858c9acfd5cab8db7e
JaronChow <jchow826@gmail.com>
2023-01-11 20:21:50 -0800

    Finished Part 1

--- db/index.js
+++ db/index.js
@@ -12,15 +12,13 @@ async function getAllUsers() {
 
 async function createUser({ username, password }) {
 try {
-    const result = await client.query(`
+    const { rows } = await client.query(`
     INSERT INTO users(username, password)
     VALUES ($1, $2)
     ON CONFLICT (username) DO NOTHING 
     RETURNING *;
     `, [username, password]);
-    const { rows } = await client.query(
-        `SELECT id, username FROM users;
-  `);
+
     return rows;
 } catch (error) {
     throw error;
