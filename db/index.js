f3695806b651763a9a7fafd1825d290daccb0111
JaronChow <jchow826@gmail.com>
2023-01-22 15:30:31 -0800

    Edited connnection

--- db/index.js
+++ db/index.js
@@ -7,7 +7,6 @@ const client = new Client({
   connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
 });
-
 async function getAllUsers() {
     const { rows } = await client.query(
       `SELECT id, username, name, location, active FROM users;`
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const {PORT = 3000, DATABASE_URL} = process.env;
+const {PORT = 3000} = process.env;
 const express = require('express');
 const server = express();
 
